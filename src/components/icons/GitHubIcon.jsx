65512cd3425a725650e40e1b34e4cda0bd4c39d8
justin-prabhakaran <c.m.justinprabhakaran@gmail.com>
2024-06-19 16:37:38 +0530

    Left,Right Icons &&Nav Added

---
+++ src/components/icons/GitHubIcon.jsx
@@ -0,0 +1,10 @@
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+
+function GitHubIcon() {
+    return (
+        <i className="fa-brands fa-github"></i>
+    )
+}
+
+export default GitHubIcon
---
+++ src/components/icons/LinkedInIcon.jsx
@@ -0,0 +1,10 @@
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+
+function LinkedInIcon() {
+    return (
+        <i className="fa-brands fa-linkedin-in"></i>
+    )
+}
+
+export default LinkedInIcon
